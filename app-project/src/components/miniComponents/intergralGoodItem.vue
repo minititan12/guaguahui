08b30dd24d909f33c731bdca3322e156712a779a
minititan <minititanzym.gmail.com>
2019-12-24 18:09:38 +0800

    积分商城商品列表

---
+++ app-project/src/components/miniComponents/intergralGoodItem.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="item-wrapper">
+    <van-image width="100%" height="40vw" src="" />
+    <div class="title">
+      <span class="text">2019新款女款np95啊哈不可发你方便</span>
+    </div>
+    <div class="item-bottom">
+      <div class="bottom-left">
+        <span class="score">2250积分</span>
+        <span class="num">已兑59915件</span>
+      </div>
+
+      <div class="btn">立即兑换</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IntergralGoodItem'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-wrapper
+    width: 43vw
+    border: 1px solid #dcdcdc
+    border-radius: 2vw
+    box-sizing: border-box
+    overflow: hidden
+    margin-bottom: 2vw
+    .title
+      width: 100%
+      padding: 2vw
+      box-sizing: border-box
+      .text
+        width: 100%
+        height: 5vw
+        font-size: 4vw
+        font-family: PFH
+        line-height: 5vw
+        text-overflow: ellipsis
+        display: -webkit-box
+        -webkit-line-clamp: 1
+        -webkit-box-orient: vertical
+        overflow: hidden
+    .item-bottom
+      padding: 0 2vw
+      padding-bottom: 3vw
+      box-sizing: border-box
+      font-family: PFB
+      font-size: 3vw
+      display: flex
+      flex-direction: row
+      justify-content: space-between
+      align-items: center
+      .bottom-left
+        display: flex
+        flex-direction: column
+        .score
+          color: #ff5756
+          margin-bottom: 1vw
+          font-size: 4vw
+        .num
+          color: #ccc
+      .btn
+        background-color: #ff5756
+        color: #fff
+        font-size: 3.6vw
+        padding: 2vw
+        border-radius: 4vw
+</style>
--- app-project/src/views/marketing/integral/integralShop.vue
+++ app-project/src/views/marketing/integral/integralShop.vue
@@ -10,32 +10,89 @@
     <div class="head">
       <div class="label">当前积分</div>
       <div class="integral">505</div>
-      <div class="tip">积分当钱花，天天都能省！</div>
+      <!-- <div class="tip">积分当钱花，天天都能省！</div> -->
       <div class="details" @click="goIntegralDetails">积分明细</div>
+    </div>
+
+    <div class="content">
+      <div class="tabs">
+        <div :class="active == index ? 'activedTab' : 'tab'" v-for="(item,index) of tabList" @click="handleChangeActive(index)">{{item}}</div>
+      </div>
+
+      <div class="goodItems" ref="goodItems">
+        <div class="items-wrapper">
+          <IntergralGoodItem />
+          <IntergralGoodItem />
+          <IntergralGoodItem />
+          <IntergralGoodItem />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import IntergralGoodItem from '../../../components/miniComponents/intergralGoodItem'
+import Bscroll from 'better-scroll'
 export default {
   name: "IntergralShop",
+  components: {
+    IntergralGoodItem
+  },
+  data(){
+    return {
+      tabList: ['全部','1-50','51-100','101-300','300以上'],
+      active: 0
+    }
+  },
   methods: {
+    initScroll(){
+      let el = this.$refs.goodItems
+      this.intergralGoodsScroll = new Bscroll(el,{
+        bounce: {
+          top: false
+        },
+        // pullUpLoad: {
+        //   threshold: 10,
+        //   stop: 0
+        // },
+        click: true,
+        eventPassthrough: 'horizontal'
+      })
+
+      this.intergralGoodsScroll.on('beforeScrollStart',()=>{
+        this.intergralGoodsScroll.refresh()
+      })
+
+      // this.groupScroll.on('pullingUp',()=>{
+      //   this.getGroupListData()
+      // })
+    },
     handleBack(){
       this.$router.go(-1)
     },
+    //跳转兑换记录
     goRedemptionRecord(){
       this.$router.push('/redemptionRecord');
     },
+    //跳转积分明细
     goIntegralDetails(){
       this.$router.push('/integralDetails');
+    },
+    //改变active
+    handleChangeActive(index){
+      this.active = index
     }
+  },
+  mounted(){
+    this.initScroll()
   }
 }
 </script>
 
 <style lang="stylus" scoped>
   .intergral-wrapper
-    background #f5f7fa
+    background #fff
     .van-nav-bar
       .van-icon
         color #FF5756
@@ -45,40 +102,89 @@ export default {
         font-size 4vw
         font-family PFH
       .van-nav-bar__text
-        font-size 3vw
+        font-size 3.3vw
         color black
+        font-family PFM
+
     .head
-      height 43vw
+      height 30vw
       position relative
       background-image url('/public/static/integral/integral_bg.png')
       background-size 100% 100%
       .details
         position absolute
         right 9vw
-        top 14vw
+        top 10vw
         background #f1f1f1
         font-size 3vw 
+        font-family PFB
         color #5b5b5b
-        font-family PFM
         height 7vw
         line-height 7vw
         border-radius 3.5vw
         padding 0 3vw
       .label
         font-size 4vw
+        font-family PFB
         color #686868
         position absolute
         left 9vw
-        top 9vw
+        top 6vw
       .integral
         font-size 9vw
+        font-family hgzt
         position absolute
         left 9vw
-        top 17vw
+        top 13vw
       .tip
         font-size 3vw
+        font-family PFM
         color #9e9e9e
         position absolute
         left 9vw
-        top 28vw
+        top 22vw
+    
+    .content
+      position: absolute 
+      top: calc(30vw + 46px)
+      left: 0
+      right: 0
+      bottom: 0
+      font-size: 3.5vw
+      .tabs
+        width: 100%
+        padding: 3vw 5vw
+        box-sizing: border-box
+        display: flex
+        flex-direction: row
+        align-items: center
+        justify-content: space-between
+        font-family: PFB
+        .tab
+          padding: 1vw 2vw
+          border: 1px solid #dcdcdc
+          border-radius: 5vw
+          color: #dcdcdc
+        .activedTab
+          padding: 1vw 2vw
+          background-color: #ffe6e6
+          color: #ff5756
+          border: 1px solid #ffe6e6
+          border-radius: 5vw
+      
+      .goodItems
+        position: absolute
+        top: 11vw
+        left: 0
+        right: 0
+        bottom: 0
+        padding: 0 5vw
+        box-sizing: border-box
+        overflow: hidden
+        .items-wrapper
+          width: 100%
+          display: flex
+          flex-direction: row
+          justify-content: space-between
+          flex-wrap: wrap
 </style>
